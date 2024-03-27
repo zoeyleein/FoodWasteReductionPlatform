2e55ca55f1779ef8a9308e01b9ed90c64d463fcb
aror0109@algonquinlive.com <aror0109@algonquinlive.com>
2024-03-26 22:34:10 -0400

    Added DTOs for all tables

---
+++ src/java/transferobjects/UserAccountDTO.java
@@ -0,0 +1,26 @@
+package transferobjects;
+
+public class UserAccountDTO {
+    private int id;
+    private double balance;
+    private int usersId;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public int getUsersId() {
+        return usersId;
+    }
+    public void setUsersId(int usersId) {
+        this.usersId = usersId;
+    }
+}
