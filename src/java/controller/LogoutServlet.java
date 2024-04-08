3ce793a4eb39d885cfea106346f2b5ed150dc0ca
JingYi Li <li000848@algonquinlive.com>
2024-04-08 09:50:43 -0400

    added logout links and servlet

--- src/java/controller/LogoutServlet.java
+++ src/java/controller/LogoutServlet.java
@@ -1,6 +1,5 @@
 package controller;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +11,11 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
-            response.sendRedirect("views/Signin.jsp");
         }
+        response.sendRedirect("views/Signin.jsp");
     }
 }
