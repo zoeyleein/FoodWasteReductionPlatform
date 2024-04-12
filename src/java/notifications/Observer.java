27e3e48e43c0888a62458662bfdf327d8ab13c06
Andres Porras <camilop1730@gmail.com>
2024-04-12 00:58:08 -0400

    Notifications working but missing category in db

--- src/java/notifications/Observer.java
+++ src/java/notifications/Observer.java
@@ -3,7 +3,9 @@ package notifications;
 import transferobjects.RetailerInventoryDTO;
 import transferobjects.UserDTO;
 
+import java.sql.Connection;
+
 public interface Observer {
-    void update(RetailerInventoryDTO retailerInventory);
+    void update(RetailerInventoryDTO retailerInventory, Connection connection);
     void setUserPreferences(String phone);
 }
--- src/java/notifications/Subject.java
+++ src/java/notifications/Subject.java
@@ -3,8 +3,10 @@ package notifications;
 import transferobjects.RetailerInventoryDTO;
 import transferobjects.UserDTO;
 
+import java.sql.Connection;
+
 public interface Subject {
     void registerObserver(Observer observer, String phone);
     void removeObserver(Observer observer);
-    void notifyObservers(RetailerInventoryDTO retailerInventory);
+    void notifyObservers(RetailerInventoryDTO retailerInventory, Connection connection);
 }
